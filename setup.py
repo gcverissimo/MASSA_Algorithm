819209d52ae28a8d2ca97aa75c469960cc69993e
Gabriel C. Veríssimo <13511582+gcverissimo@users.noreply.github.com>
2024-06-04 18:27:57 -0300

    Updated setup.py requirements to match the README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as readme:
 
 setuptools.setup(
 	name='MASSA Algorithm',
-	version='0.9.2',
+	version='0.9.2a',
 	description='MASSA Algorithm is a Python package to separate data sets of molecules into training and test sets, considering the diversity of structural, physicochemical and biological characteristics of these molecules.',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
@@ -15,8 +15,8 @@ setuptools.setup(
 	license='AGPLv3',
 	packages= setuptools.find_packages(),
 	entry_points={'console_scripts':['MASSA_Algorithm = MASSA_Algorithm.MASSA:main']},
-	install_requires=['numpy', 'rdkit-pypi', 'pandas', 'matplotlib >= 3.2', 'scipy >= 1.6', 'scikit-learn >= 0.24', 'kmodes >= 0.10'],
-	python_requires='>=3.8, <4',
+	install_requires=['numpy', 'rdkit-pypi', 'pandas', 'matplotlib >= 3.2', 'scipy >= 1.6', 'scikit-learn = 0.24', 'kmodes >= 0.10'],
+	python_requires='>=3.8, <3.10',
 	keywords=['chemoinformatics', 'training', 'test', 'training-test', 'dataset preparation', 'data set preparation', 'rdkit'],
 	classifiers=['Programming Language :: Python :: 3',
 	'Programming Language :: Python :: 3.8',
